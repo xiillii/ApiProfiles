b56d690ce37785991bc060c9ccb2b2fa9b565822
Jose Alonso <mail@josealonso.dev>
2023-09-20 16:00:44 -0600

    Domain Fix EntityBase

--- src/core/ApiPersonProfiles.Core.Domain/Common/EntityBase.cs
+++ src/core/ApiPersonProfiles.Core.Domain/Common/EntityBase.cs
@@ -3,6 +3,6 @@
 public abstract class EntityBase
 {
     public int Id { get; set; }
-    public DateTimeOffset DateCreated { get; set; }
-    public DateTimeOffset DateUpdated { get; set; }
+    public DateTimeOffset? DateCreated { get; set; }
+    public DateTimeOffset? DateUpdated { get; set; }
 }
