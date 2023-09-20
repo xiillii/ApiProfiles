5348b44f60bc7913aa1cc77aa697021642f6fafd
Jose Alonso <mail@josealonso.dev>
2023-09-20 17:26:40 -0600

    Application. Services registration extension

---
+++ src/core/ApiPersonProfiles.Core.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ApiPersonProfiles.Core.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        services.AddAutoMapper(assembly);
+        services.AddMediatR(configuration =>
+                    configuration.RegisterServicesFromAssemblies(assembly));
+
+        return services;    
+    }
+}
