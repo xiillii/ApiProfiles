710815700b58ea4c2182e040bf8679a1ce034447
Jose Alonso <mail@josealonso.dev>
2023-09-19 22:19:46 -0600

    Application. Initial Repository Interfaces

---
+++ src/core/ApiPersonProfiles.Core.Application/Contracts/Persistence/IPictureRepository.cs
@@ -0,0 +1,7 @@
+﻿using ApiPersonProfiles.Core.Domain;
+
+namespace ApiPersonProfiles.Core.Application.Contracts.Persistence;
+
+public interface IPictureRepository : IGenericRepository<Picture>
+{
+}
---
+++ src/core/ApiPersonProfiles.Core.Application/Contracts/Persistence/IProfileRepository.cs
@@ -0,0 +1,7 @@
+﻿using ApiPersonProfiles.Core.Domain;
+
+namespace ApiPersonProfiles.Core.Application.Contracts.Persistence;
+
+public interface IProfileRepository : IGenericRepository<Profile>
+{
+}
