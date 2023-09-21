dc42a8dd6e2a711c9fc94b669bdab0b02da8cc23
Jose Alonso <mail@josealonso.dev>
2023-09-20 23:21:11 -0600

    Application. Profile Feature. Delete Command

---
+++ src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/DeleteProfile/DeleteProfileCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ApiPersonProfiles.Core.Application.Features.Profile.Commands.DeleteProfile;
+
+public class DeleteProfileCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
---
+++ src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/DeleteProfile/DeleteProfileCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using ApiPersonProfiles.Core.Application.Contracts.Persistence;
+using AutoMapper;
+using MediatR;
+
+namespace ApiPersonProfiles.Core.Application.Features.Profile.Commands.DeleteProfile;
+
+public class DeleteProfileCommandHandler : IRequestHandler<DeleteProfileCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly IProfileRepository _repository;
+
+    public DeleteProfileCommandHandler(IMapper mapper, IProfileRepository repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public async Task<Unit> Handle(DeleteProfileCommand request, CancellationToken cancellationToken)
+    {
+        // get the data from database
+        var profileToDelete = await _repository.GetByIdAsync(request.Id);
+
+        if (profileToDelete == null)
+        {
+            // TODO: Return a custom notfound error
+            throw new Exception();
+        }
+
+        // delete from database
+        await _repository.DeleteAsync(profileToDelete);
+
+        // return nothing
+        return Unit.Value;
+    }
+}
