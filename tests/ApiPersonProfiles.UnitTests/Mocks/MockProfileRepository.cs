3169a15bcbb713e2819a67b87df034aebb9bb81a
Jose Alonso <mail@josealonso.dev>
2023-09-20 18:10:07 -0600

    Tests. Setup Mocks

---
+++ tests/ApiPersonProfiles.UnitTests/Mocks/MockProfileRepository.cs
@@ -0,0 +1,53 @@
+﻿using ApiPersonProfiles.Core.Application.Contracts.Persistence;
+using ApiPersonProfiles.Core.Domain;
+using Moq;
+
+namespace ApiPersonProfiles.UnitTests.Mocks;
+
+public class MockProfileRepository
+{
+    public static Mock<IProfileRepository> GetProfileRepository()
+    {
+        var profile = new Profile
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe",
+            Age = 10,
+            Nickname = "fulanito"
+        };
+
+        var profiles = new List<Profile>
+        {
+            new Profile
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 10,
+                Nickname = "fulanito"
+            },
+            new Profile
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Age = 20,
+                Nickname = "fulanita"
+            },
+        };
+
+        var mockRepo = new Mock<IProfileRepository>();
+
+        mockRepo.Setup(r => r.GetAsync()).ReturnsAsync(profiles);
+        mockRepo.Setup(r => r.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(profile);
+        mockRepo.Setup(r => r.CreateAsync(It.IsAny<Profile>())).Returns((Profile profile) =>
+        {
+            profiles.Add(profile);
+
+            return Task.CompletedTask;
+        });
+
+        return mockRepo;
+    }
+}
