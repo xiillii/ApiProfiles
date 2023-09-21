293c15f93415933178e56a643ed965c142965d50
Jose Alonso <mail@josealonso.dev>
2023-09-20 22:14:22 -0600

    Application. Profile Feature. Commands. Create

---
+++ src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/CreateProfile/CreateProfileCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace ApiPersonProfiles.Core.Application.Features.Profile.Commands.CreateProfile;
+
+public class CreateProfileCommand :IRequest<int>
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string? MyProperty { get; set; } = string.Empty;
+}
---
+++ src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/CreateProfile/CreateProfileCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using ApiPersonProfiles.Core.Application.Contracts.Persistence;
+using AutoMapper;
+using MediatR;
+
+namespace ApiPersonProfiles.Core.Application.Features.Profile.Commands.CreateProfile;
+
+public class CreateProfileCommandHandler : IRequestHandler<CreateProfileCommand, int>
+{
+    private readonly IMapper _mapper;
+    private readonly IProfileRepository _repository;
+
+    public CreateProfileCommandHandler(IMapper mapper, IProfileRepository repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public async Task<int> Handle(CreateProfileCommand request, CancellationToken cancellationToken)
+    {
+        // TODO: validate incomnig data
+
+        // Convert DTO to domain entity
+        var profileToCreate = _mapper.Map<Domain.Profile>(request);
+
+        // add to database
+        await _repository.CreateAsync(profileToCreate);
+
+        // return id
+        return profileToCreate.Id;
+    }
+}
