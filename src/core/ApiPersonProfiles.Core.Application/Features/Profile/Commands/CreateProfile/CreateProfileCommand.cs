aa591ea7ce6a72de108851f5566a9c1449411976
Jose Alonso <mail@josealonso.dev>
2023-09-21 20:39:03 -0600

    Application. Fix Create Command
    Typo Nickname

--- src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/CreateProfile/CreateProfileCommand.cs
+++ src/core/ApiPersonProfiles.Core.Application/Features/Profile/Commands/CreateProfile/CreateProfileCommand.cs
@@ -7,5 +7,5 @@ public class CreateProfileCommand :IRequest<int>
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public int Age { get; set; }
-    public string? MyProperty { get; set; } = string.Empty;
+    public string? Nickname { get; set; } = string.Empty;
 }
