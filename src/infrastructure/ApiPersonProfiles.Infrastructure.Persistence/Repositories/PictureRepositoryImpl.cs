94384b5436bb9136320aa583f750ad6fe4deff5a
Jose Alonso <mail@josealonso.dev>
2023-09-22 22:24:37 -0600

    Persistence. Implement Picture Repository

---
+++ src/infrastructure/ApiPersonProfiles.Infrastructure.Persistence/Repositories/PictureRepositoryImpl.cs
@@ -0,0 +1,19 @@
+﻿using ApiPersonProfiles.Core.Application.Contracts.Persistence;
+using ApiPersonProfiles.Core.Domain;
+using ApiPersonProfiles.Infrastructure.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonProfiles.Infrastructure.Persistence.Repositories;
+
+// TODO: Implement the file upload to a file repository
+public class PictureRepositoryImpl : GenericRepositoryImpl<Picture>, IPictureRepository
+{
+    public PictureRepositoryImpl(EFDatabaseContext context) : base(context)
+    {
+    }
+
+    public async Task<Picture> GetByProfileIdAsync(int profileId)
+    {
+        return await _context.Pictures.FirstOrDefaultAsync(p => p.ProfileId == profileId);
+    }
+}
